bf5ab19003de2e88e73cda750ec0088801e69603
enzo <enzoap2009@hotmail.com>
2021-01-07 03:24:26 -0300

    Organizado as pastas

--- app/src/main/java/com/example/nybooks/presentation/books/BooksActivity.kt
+++ app/src/main/java/com/example/nybooks/presentation/books/BooksActivity.kt
@@ -1,11 +1,12 @@
-package com.example.nybooks
+package com.example.nybooks.presentation.books
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nybooks.R
 
-class MainActivity : AppCompatActivity() {
+class BooksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_books)
     }
 }
